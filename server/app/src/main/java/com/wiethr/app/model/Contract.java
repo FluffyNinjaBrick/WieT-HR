4677972774ac86478e2fea4c98a20c7782955614
FluffyNinjaBrick <1111do.gier1111@gmail.com>
2021-03-29 12:52:30 +0200

    repos done, database init in progress

--- server/app/src/main/java/com/wiethr/app/model/Contract.java
+++ server/app/src/main/java/com/wiethr/app/model/Contract.java
@@ -4,10 +4,7 @@ import com.wiethr.app.model.enums.ContractType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
