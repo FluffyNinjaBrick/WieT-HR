fb6f46fa10dfe7689fa38476275a98b403519499
tkozyra <tomekkozyra@gmail.com>
2021-05-04 10:43:08 +0200

    Add basic data to user profile

--- frontend/app/src/components/profile/Profile.js
+++ frontend/app/src/components/profile/Profile.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import { fetchCurrentEmployee } from "../../services/EmployeeService";
+import { Loading } from "../loader/LoadingView";
+import ProfileData from "./ProfileData";
+
 export default function Profile() {
+  const [employee, setEmployee] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetchCurrentEmployee()
+      .then((data) => setEmployee(data))
+      .then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className="container">
-      <h1 className="my-3">Profil</h1>
+      <h1 className="my-3 mb-5">Profil</h1>
+      {employee ? (
+        <div>
+          <ProfileData
+            title="Imię i nazwisko"
+            value={employee.firstName + " " + employee.lastName}
+          />
+          <ProfileData title="Adres e-email" value={employee.email} />
+          <ProfileData title="Numer telefonu" value={employee.phone} />
+          <ProfileData title="Adres" value={employee.address} />
+        </div>
+      ) : null}
     </div>
   );
 }
--- frontend/app/src/services/EmployeeService.js
+++ frontend/app/src/services/EmployeeService.js
@@ -22,3 +22,23 @@ export const fetchEmployees = async () => {
   const employees = await response.json();
   return employees;
 };
+
+export const fetchCurrentEmployee = async () => {
+  const user = JSON.parse(getCurrentUser());
+  const token = user ? user.jwt : "";
+  const auth = "Bearer " + token;
+  const id = user.id;
+
+  const myHeaders = {
+    "Content-Type": "application/json",
+    Authorization: auth,
+  };
+
+  const response = await fetch(API_URL + "employees/" + id, {
+    method: "GET",
+    headers: myHeaders,
+  });
+
+  const employees = await response.json();
+  return employees;
+};
